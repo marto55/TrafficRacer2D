b92798a4bd4273194817c3fa297f4c39987d3b55
marto55 <martin_my@abv.bg>
2023-03-12 11:10:05 +0200

    add first version of the game, functional but not OOP

---
+++ CarRacingWPFApp/CarRacingWPFApp/MenuWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarRacingWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startGameButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            GameWindow gameWindow = new GameWindow();
+            gameWindow.Show();
+        }
+
+        private void garageButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            GarageWindow garageWindow = new GarageWindow();
+            garageWindow.Show();
+        }
+    }
+}
